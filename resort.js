f25ca042461e928447bdd3487841e92aafa396ac
Sharon <91304379+swindless@users.noreply.github.com>
2021-10-19 23:06:52 -0500

    add new files

--- resort.js
+++ resort.js
@@ -6,7 +6,12 @@ function say_name() {
     var greeting = 'Hello ' + fName + ' ' + lName + '. ' + 'We will contact you at ' + eAddress ;
   
     document.getElementById('result').innerHTML = greeting;
+    {
+        return alert('Are you sure you want to submit?');
+    }
+
 }
- 
 document.getElementById('send').addEventListener('click', say_name);
+ 
+
 		
