7d5de296742b30af1927177c700c79ad1b6c9e92
Sharon <91304379+swindless@users.noreply.github.com>
2022-01-24 16:03:13 -0600

    add file

--- schooldate.js
+++ schooldate.js
@@ -1,22 +1,5 @@
-var today = new Date();
-var dd = today.getDate();
-
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-if(dd<10)
-{
-    dd='0'+dd;
-}
-
-if(mm<10)
-{
-    mm='0'+mm;
-}
-today = mm+'-'+dd+'-'+yyyy;
-console.log(today);
-today = mm+'/'+dd+'/'+yyyy;
-console.log(today);
-today = dd+'-'+mm+'-'+yyyy;
-console.log(today);
-today = dd+'/'+mm+'/'+yyyy;
-console.log(today);
+var currentDate = new Date(),
+        day = currentDate.getDate(),
+        month = currentDate.getMonth() + 1,
+        year = currentDate.getFullYear();
+    document.write("Today is "+ month + "/" + day + "/" + year)
